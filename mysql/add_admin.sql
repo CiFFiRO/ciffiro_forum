55c72b10ba5491568ff4c178ee8f85ea95674177
CiFFiRO <nikitazatsepin96@yandex.ru>
2021-04-07 23:42:07 +0300

    mysql: add script for add admin

---
+++ mysql/add_admin.sql
@@ -0,0 +1,2 @@
+INSERT INTO `forum`.`user` (`email`, `nickname`, `password_hash`, `is_admin`, `registration_date`, `last_activity`)
+VALUES ('', 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 1, CURDATE(), NOW());
